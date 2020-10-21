d6a2de602e92bec128de0aafe87af9efea42bd90
Oleksandr Verhun <stranger9983@gmail.com>
2020-10-21 12:15:28 +0300

    feat(add dist): add dist

---
+++ dist/langs/fr.js
@@ -0,0 +1,1 @@
+export default{days:["Dim","Lun","Mar","Mer","Jeu","Ven","Sam"],daysShort:["Di","Lu","Ma","Me","Je","Ve","Sa"],months:["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"],monthsShort:["Jan","Fév","Mar","Avr","Mai","Jun","Jul","Aoû","Sep","Oct","Nov","Déc"]};
